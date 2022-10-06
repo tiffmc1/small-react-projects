6e25fc04376cf0c627590c63da991f8ac85bc5e0
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2022-10-06 14:45:18 -0400

    DataFetch: fetched and displayed user data on page

--- src/projects/dataFetch/DataFetch.js
+++ src/projects/dataFetch/DataFetch.js
@@ -1,5 +1,38 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+// Use the api below to fetch this user's data and display it on the web page
+// https://randomuser.me/api
+
+const fetchUserData = async () => {
+	return await axios
+		.get("https://randomuser.me/api")
+		.then(({ data }) => {
+			console.log(data.results);
+			return JSON.stringify(data, null, 2); // can just return the data object, but including "null, 2," along with the "pre" tag, formats the data in an easier-to-read way. Delete these factors and see how the data is presented
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+};
+
 const DataFetch = () => {
-	return <div>this is data fetch page</div>;
+	const [randomUserData, setRandomUserData] = useState("");
+
+	useEffect(() => {
+		fetchUserData().then((userData) => {
+			setRandomUserData(userData || "No User Data");
+		});
+	}, []);
+
+	return (
+		<>
+			<pre>{randomUserData}</pre>
+			{/* {randomUserData.results.map((user, idx) => (
+				<div key={idx}>{user.name}</div>
+			))} */}
+		</>
+	);
 };
 
 export default DataFetch;
