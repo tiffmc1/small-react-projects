d293a03a41e6c19566cc4c6aa91bd6b1b6bb182d
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2022-10-06 16:09:37 -0400

    DataFetch: append new users to state

--- src/projects/dataFetch/DataFetch.js
+++ src/projects/dataFetch/DataFetch.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { fetchUserData } from "./api";
 
 // 1. Use the api below to fetch this user's data and display it on the web page
@@ -10,13 +10,22 @@ import { fetchUserData } from "./api";
 const DataFetch = () => {
 	const [randomUserJSON, setRandomUserJSON] = useState("");
 	const [userResults, setUserResults] = useState([]);
+	const [nextPageNum, setNextPageNum] = useState(1);
 
-	useEffect(() => {
-		fetchUserData().then((userData) => {
+	const fetchNextUser = useCallback(() => {
+		fetchUserData(nextPageNum).then((userData) => {
 			setRandomUserJSON(JSON.stringify(userData, null, 2) || "No User Data");
 			// can just return the data object, but by including "null, 2," along with the "pre" tag below, this formats the data in an easier-to-read way. Delete these factors and see how the data is presented
-			setUserResults(userData.results);
+
+			const appendUser = [...userResults, ...userData.results];
+			setUserResults(appendUser);
+
+			setNextPageNum(userData.info.page + 1);
 		});
+	}, [nextPageNum, userResults]);
+
+	useEffect(() => {
+		fetchNextUser();
 	}, []);
 
 	return (
@@ -30,8 +39,8 @@ const DataFetch = () => {
 						</div>
 					</div>
 				))}
+				<button onClick={fetchNextUser}>Next User</button>
 				<pre>{randomUserJSON}</pre>
-				<button>Next User</button>
 			</div>
 		</>
 	);
--- src/projects/dataFetch/api.js
+++ src/projects/dataFetch/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-export const fetchUserData = async () => {
+export const fetchUserData = async (pageNumber) => {
 	return await axios
-		.get("https://randomuser.me/api")
+		.get(`https://randomuser.me/api?page=${pageNumber}`)
 		.then(({ data }) => {
 			//console.log(data);
 			return data;
