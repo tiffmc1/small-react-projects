65ac5de4d7df59f0f7c5b453567d484924cabf90
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2022-10-06 20:01:37 -0400

    New Project: Data Fetching Albums

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
 				<Route path="/" element={<Home />} />
 				<Route path="/counter" element={<Counter />} />
 				<Route path="/dataFetch" element={<DataFetch />} />
+				<Route path="/albums" />
 			</Routes>
 		</>
 	);
--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,8 @@ const Home = () => {
 	return (
 		<div className="home-container">
 			<Link to="/counter">Increment Count</Link>
-			<Link to="dataFetch">Data Fetching</Link>
+			<Link to="/dataFetch">Data Fetching: Random Users</Link>
+			<Link to="/albums">Data Fetching: Albums</Link>
 		</div>
 	);
 };
