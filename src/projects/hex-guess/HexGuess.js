f1d39bcde5f191daec52ed06c52c01f5d943670f
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2022-10-14 16:29:39 -0400

    HexGuess: reloads if correct color is guessed

--- src/projects/hex-guess/HexGuess.js
+++ src/projects/hex-guess/HexGuess.js
@@ -1,12 +1,22 @@
 import { useState, useEffect } from "react";
 import "../hex-guess/hexGuess.css";
 
-const HexGuess = () => {
+const generateRandomColor = () => {
+	const red = parseInt(Math.random() * 255);
+	const green = parseInt(Math.random() * 255);
+	const blue = parseInt(Math.random() * 255);
+
+	return `#${red.toString(16).toUpperCase()}${green
+		.toString(16)
+		.toUpperCase()}${blue.toString(16).toUpperCase()}`;
+};
+
+function HexGuess() {
 	const [color, setColor] = useState("");
 	const [colors, setColors] = useState([]);
 	const [isWrong, setIsWrong] = useState(null);
 
-	useEffect(() => {
+	const generateColors = () => {
 		const displayedColor = generateRandomColor();
 		const wrongColor1 = generateRandomColor();
 		const wrongColor2 = generateRandomColor();
@@ -15,20 +25,17 @@ const HexGuess = () => {
 		setColors(
 			[displayedColor, wrongColor1, wrongColor2].sort(() => Math.random() - 0.5)
 		);
-	}, []);
-
-	const generateRandomColor = () => {
-		const red = parseInt(Math.random() * 255);
-		const green = parseInt(Math.random() * 255);
-		const blue = parseInt(Math.random() * 255);
-
-		return `#${red.toString(16).toUpperCase()}${green
-			.toString(16)
-			.toUpperCase()}${blue.toString(16).toUpperCase()}`;
 	};
 
+	useEffect(() => {
+		generateColors();
+	}, []);
+
 	const handleAnswer = (guessedColor) => {
-		guessedColor === color ? setIsWrong(false) : setIsWrong(true);
+		if (guessedColor === color) {
+			setIsWrong(false);
+			generateColors();
+		} else setIsWrong(true);
 	};
 
 	return (
@@ -59,6 +66,6 @@ const HexGuess = () => {
 			</div>
 		</>
 	);
-};
+}
 
 export default HexGuess;
