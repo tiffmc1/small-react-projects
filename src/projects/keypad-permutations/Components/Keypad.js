3dd3033830b6707d609df3187dcdbfcf9e5bdc17
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2022-11-23 12:14:49 -0500

    Keypad: bare bones design/styling added

--- src/projects/keypad-permutations/Components/Keypad.js
+++ src/projects/keypad-permutations/Components/Keypad.js
@@ -1,7 +1,26 @@
-import React from "react";
+import "../stylesheets/keypad.css";
 
 const Keypad = () => {
-	return <div>Keypad</div>;
+	return (
+		<>
+			<div className="keypad-container">
+				<div className="keypad-wrapper">
+					<div className="keys-container">
+						<button className="key">1</button>
+						<button className="key">2</button>
+						<button className="key">3</button>
+						<button className="key">4</button>
+						<button className="key">5</button>
+						<button className="key">6</button>
+						<button className="key">7</button>
+						<button className="key">8</button>
+						<button className="key">9</button>
+						<button className="key">0</button>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Keypad;
