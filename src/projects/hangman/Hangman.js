5526085e638aff7a1904900f3bd00133931f2dcf
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2023-01-30 19:31:13 -0500

    Hangman: added functionality for right/wrong guess

--- src/projects/hangman/Hangman.js
+++ src/projects/hangman/Hangman.js
@@ -30,6 +30,10 @@ const alphabet = [
 	"z",
 ];
 
+let dummyWord = "einstein";
+let dummyWordArr = dummyWord.split("");
+const underscore = " _____ ";
+
 const Hangman = () => {
 	const [dummy, setDummy] = useState([
 		{ letter: "e", correctlyGuessed: false },
@@ -41,23 +45,42 @@ const Hangman = () => {
 		{ letter: "i", correctlyGuessed: false },
 		{ letter: "n", correctlyGuessed: false },
 	]);
+	const [chances, setChances] = useState(10);
+	const [incorrectGuesses, setIncorrectGuesses] = useState([]);
 
 	const handleLetterGuess = (guessedLetter) => {
+		// setting correctly guessed letters to state
 		let copyDummy = [...dummy];
+		copyDummy.map((char) =>
+			guessedLetter === char.letter ? (char.correctlyGuessed = true) : null
+		);
+		setDummy(copyDummy);
 
-		for (let char in copyDummy) {
-			let letter = copyDummy[char];
+		// incorrect guesses/decrease chances
+		let isIncorrectGuess = !dummyWordArr.includes(guessedLetter);
+		if (isIncorrectGuess) {
+			setChances(chances - 1);
+			setIncorrectGuesses((prevGuesses) => [...prevGuesses, guessedLetter]);
 
-			if (guessedLetter === letter.letter) {
-				letter.correctlyGuessed = true;
-				setDummy(copyDummy);
+			if (chances <= 0) {
+				setTimeout(() => {
+					alert("You lose");
+				}, 1000);
 			}
+		}
+
+		// winner!
+		if (dummy.every((letter) => letter.correctlyGuessed === true)) {
+			setTimeout(() => {
+				alert("congrats you won");
+			}, 1000);
 		}
 	};
 
 	return (
 		<div>
 			<div>Hangman</div>
+
 			<div className="hangman-alphabet">
 				{alphabet.map((letter, i) => (
 					<button
@@ -69,20 +92,28 @@ const Hangman = () => {
 					</button>
 				))}
 			</div>
+
 			<div className="hangman-category">The category is famous scientists</div>
+
 			<div className="hangman-underscores">
 				{dummy.map((char, i) => (
 					<div key={i} className="hangman-underscore">
-						{char.correctlyGuessed ? char.letter : "_____"}
+						{char.correctlyGuessed ? char.letter : underscore}
 					</div>
 				))}
 			</div>
+
+			<div>Chances: {chances}</div>
+			<div>Incorrect Guesses: {incorrectGuesses.join(", ")}</div>
+
 			<img
-				src={require("./images/hangman-10.png")}
+				src={require("./images/hangman-9.png")}
 				alt=""
 				height={350}
 				width={250}
 			/>
+
+			<button>Play Again</button>
 		</div>
 	);
 };
